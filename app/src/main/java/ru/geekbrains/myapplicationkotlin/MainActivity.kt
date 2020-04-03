f39887a49b395dcc24ec0663386e870879165a72
Artem <kunakbaevav@yandex.ru>
2020-04-03 10:57:55 +0500

    Домашнее задание к уроку 1 Сделал кнопку и повесил на нее listener

--- app/src/main/java/ru/geekbrains/myapplicationkotlin/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/myapplicationkotlin/MainActivity.kt
@@ -2,11 +2,25 @@ package ru.geekbrains.myapplicationkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    val myClickListener = View.OnClickListener {
+        v ->  showMessage(v)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.myButton)
+        button.setOnClickListener(myClickListener)
+    }
+
+    private fun showMessage(view: View){
+        val myToast = Toast.makeText(view.context, "Есть контакт!", Toast.LENGTH_SHORT)
+        myToast.show()
     }
 }
